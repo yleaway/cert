3b7b0a00012111b94df4aae26887d0217ef8a356
yleaway <37989139+yleaway@users.noreply.github.com>
2024-03-11 19:06:25 +0800

    Update convert.sh
    添加本地固件支持

--- convert.sh
+++ convert.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-# Prompt the user to input the firmware download URL
-read -p "请输入固件下载网址: " firmware_url
+# Prompt the user to input the firmware download URL or local path
+read -p "请输入固件下载地址或者固件本地路径: " firmware_path
 
-# Extract filename from URL
-filename=$(basename "$firmware_url")
+# Check if the input is a URL or local path
+if [[ "$firmware_path" =~ ^https?:// || "$firmware_path" =~ ^/ ]]; then
+    # If the input is a URL, download the firmware file
+    if [[ "$firmware_path" =~ ^https?:// ]]; then
+        # Extract filename from URL
+        filename=$(basename "$firmware_path")
 
-# Download the firmware file
-wget "$firmware_url" -P /root/
+        # Download the firmware file
+        wget "$firmware_path" -P /root/
 
-# Check if the downloaded file is a .img or .img.gz file
-if [[ "$filename" == *.img ]]; then
-    firmware_file="/root/$filename"
-elif [[ "$filename" == *.img.gz ]]; then
-    # Extract the .img.gz file to .img
-    gunzip "/root/$filename"
-    firmware_file="/root/${filename%.gz}"
+        # Check if the downloaded file is a .img or .img.gz file
+        if [[ "$filename" == *.img ]]; then
+            firmware_file="/root/$filename"
+        elif [[ "$filename" == *.img.gz ]]; then
+            # Extract the .img.gz file to .img
+            gunzip "/root/$filename"
+            firmware_file="/root/${filename%.gz}"
+        else
+            echo "不支持的文件格式。"
+            exit 1
+        fi
+    else
+        # If the input is a local path, set firmware_file to the specified path
+        firmware_file="$firmware_path"
+    fi
 else
-    echo "不支持的文件格式。"
+    echo "无效的输入。请输入固件下载地址或者固件本地路径。"
     exit 1
 fi
 
@@ -31,10 +43,10 @@ mount -o loop,offset=$root_partition "$firmware_file" /root/op
 cd /root/op || exit 1
 
 # Get the base name of the file without extension
-filename=$(basename "$firmware_url" | sed 's/\.img\.gz$//')
+filename=$(basename "$firmware_file" | sed 's/\(\.img\|\.img\.gz\|\.gz\)$//')
 
 # Create a tar.gz archive of the files and move it to the template cache directory
-tar zcf "/var/lib/vz/template/cache/$filename.tar.gz" * 
+tar zcf "/var/lib/vz/template/cache/$filename.tar.gz" *
 
 # Move back to the original directory
 cd ..
