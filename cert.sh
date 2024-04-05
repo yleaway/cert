f91d7306a505f43b484c72fab7cb550368ec9a33
yleaway <37989139+yleaway@users.noreply.github.com>
2024-04-05 19:28:30 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,25 +1,58 @@
 #!/bin/bash
 
-# Function to renew certificate
-renew_certificate() {
-    certbot renew
+# Function to stop Nginx service if it is running
+stop_nginx() {
+    if systemctl is-active --quiet nginx; then
+        systemctl stop nginx
+    else
+        echo "Nginx is not running. Skipping stop."
+        return 0
+    fi
 }
+
+# Create directory if it doesn't exist
+if [ ! -d /root/cert/ ] || [ ! -d /root/.secrets/ ]; then
+    mkdir -p /root/cert/ /root/.secrets/
+fi
+
+# Stop Nginx service if it is running
+stop_nginx
 
 # Function to generate certificate using Standalone mode
 generate_standalone_certificate() {
     read -p "Enter domain name: " domain
-    certbot certonly --standalone -d $domain
+    read -p "Enter email address: " email
+    ~/.acme.sh/acme.sh \
+        --register-account -m $email \
+        --issue -d $domain --standalone \
+        --installcert -d $domain \
+        --key-file /root/cert/private.key \
+        --fullchain-file /root/cert/fullchain.crt
+        
 }
 
 # Function to generate certificate using DNS mode with Cloudflare
 generate_dns_certificate() {
     read -p "Enter domain name: " domain
-    read -p "Enter Cloudflare email: " cloudflare_email
-    read -p "Enter Cloudflare API key: " cloudflare_api_key
-    certbot certonly --dns-cloudflare --dns-cloudflare-credentials ~/.secrets/cloudflare.ini -d $domain
+    read -p "Enter Cloudflare API Token: " cloudflare_api_token
+
+    # Export CF_Token
+    export CF_Token="$cloudflare_api_token"
+
+    ~/.acme.sh/acme.sh \
+        --register-account -m $email \
+        --issue --dns dns_cf \
+        -d $domain \
+        -d *.$domain \
+        --installcert -d $domain \
+        --key-file /root/cert/private.key \
+        --fullchain-file /root/cert/fullchain.crt
+        
 }
 
 # Main script
+
+# Choose certificate generation mode
 echo "Choose certificate generation mode:"
 echo "1. Standalone"
 echo "2. DNS (Cloudflare)"
@@ -38,14 +71,6 @@ case $mode in
         ;;
 esac
 
-# Copy generated certificate to /root/cert directory
-cert_dir="/root/cert"
-if [ ! -d "$cert_dir" ]; then
-    mkdir -p "$cert_dir"
-fi
-
-cp /etc/letsencrypt/live/*/fullchain.pem "$cert_dir/fullchain.crt"
-cp /etc/letsencrypt/live/*/privkey.pem "$cert_dir/private.key"
-
 # Renew certificate every 60 days
-(crontab -l ; echo "0 0 */60 * * /root/renew_certificate.sh") | crontab -
+(crontab -l ; echo '0 0 */60 * * service nginx stop && ~/.acme.sh/acme.sh --cron --force && service nginx start') | crontab -
+
