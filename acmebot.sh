286c9da94dec56f3650634076adae8abb2e38768
yleaway <37989139+yleaway@users.noreply.github.com>
2024-06-09 21:10:58 +0800

    Update acmebot.sh

--- acmebot.sh
+++ acmebot.sh
@@ -113,4 +113,5 @@ case $mode in
 esac
 
 # Add cron job for acme.sh certificate renewal
-# (crontab -l ; echo '00 3 * * * "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" > /dev/null') | crontab -
+cron_job='00 3 * * * "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" > /dev/null'
+(crontab -l | grep -F "$cron_job") || (crontab -l ; echo "$cron_job") | crontab -
